584c92483838f0f842cdb4db5286eb90cb848edf
felipesouzabd <felipesbd89@gmail.com>
2024-05-11 15:49:19 -0300

    Update BVN.py

--- BVN.py
+++ BVN.py
@@ -2,4 +2,6 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+df = pd.read_excel('ONS-Acomph.xlsx', sheet_name= "Acomph")
+
 st.title('BVN - Boletim de Vazões e Níveis do Complexo Belo Monte')
